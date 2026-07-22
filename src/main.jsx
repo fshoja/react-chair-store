45084436819e87a165f9a78dab9014d40ec6d7a0
fshoja <fshoja56@gmail.com>
2026-07-22 04:26:04 -0700

    Add basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
+<BrowserRouter basename='/react-chair-store'>
+  <StrictMode>
     <App />
-  </StrictMode>,
-  </BrowserRouter>
+  </StrictMode>
+</BrowserRouter>
 
 )
